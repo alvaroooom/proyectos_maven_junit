359358709a35b41a98a305fb220b5362185c23f9
alvaroooom <munoz.fernandez.alvaro@iescamas.es>
2025-01-21 13:13:40 +0100

    Ejercicio casi terminado (termometro)

--- termometro_proyecto/src/test/java/instrumento/TermometroTest.java
+++ termometro_proyecto/src/test/java/instrumento/TermometroTest.java
@@ -14,8 +14,24 @@ class TermometroTest {
 
 	@BeforeEach
 	void setUp() throws Exception {
-		termometro = new Termometro(5.0);
+		termometro = new Termometro(5.0);	
+	}
+	
+	@Test
+	@DisplayName("Prueba del constructor")
+	void testPruebaConstructor() {
+		termometro = new Termometro(5);
+		Double valorEsperado = (double) 5;
+		Double valor = termometro.getTemperaturaCelsius();
 		
+		assertEquals(valorEsperado, valor);
+	}
+	
+	@Test
+	@DisplayName("Poner Temperatura")
+	void testSetTemperatura() {
+		termometro.setTemperaturaCelsius(30.0);
+        assertEquals(30.0, termometro.getTemperaturaCelsius());
 	}
 
 	@Test
@@ -23,7 +39,14 @@ class TermometroTest {
 	void testConvertirFahrenheit() {
 		Double valorEsperado = 41.0;
 		Double valor = termometro.convertirAFahrenheit();
-		
+		assertEquals(valorEsperado, valor);
+	}
+	
+	@Test
+	@DisplayName("Temperatura Kelvin válida")
+	void testKelvinVálido() {
+		Double valorEsperado = 278.15;
+		Double valor = termometro.convertirAKelvin();
 		assertEquals(valorEsperado, valor);
 	}
 	
@@ -38,13 +61,67 @@ class TermometroTest {
 	}
 	
 	@Test
-	@DisplayName("Temperatura Kelvin Válida")
-	void testKelvinVálido() {
-		Double valorEsperado = 278.15;
-		Double valor = termometro.convertirAKelvin();
+	@DisplayName("Temperatura no en rango")
+	void testTemperaturaNoEnRango() {
+		assertFalse(termometro.estaEnRango(10, 20), "No está en rango");
+	}
+	
+	@Test
+	@DisplayName("Temperatura en rango")
+	void testTemperaturaEnRango() {
+		assertTrue(termometro.estaEnRango(0, 10));
+	}
+
+	@Test
+	@DisplayName("Temperatura en congelación")
+	void testTemperaturaEnCongelación() {
+		termometro = new Termometro (-5);
+		assertTrue(termometro.esCongelacion());
+	}
+	
+	@Test
+	@DisplayName("Temperatura no en congelación")
+	void testTemperaturaEnNoCongelación() {
+		assertFalse(termometro.esCongelacion());
+	}
+	
+	@Test
+	@DisplayName("Temperatura en ebullición")
+	void testTemperaturaEnEbullición() {
+		termometro = new Termometro (180);
+		assertTrue(termometro.esEbullicion());
+	}
+	
+	@Test
+	@DisplayName("Temperatura no en ebullición")
+	void testTemperaturaNoEnEbullición() {
+		assertFalse(termometro.esEbullicion());
+	}
+	
+	@Test
+	@DisplayName("Ajustar temperatura")
+	void testAjustarTemperatura() {
+		Double valorEsperado = 40.0;
+		termometro.ajustarTemperatura(35);
+		Double valor = termometro.getTemperaturaCelsius();
 		
 		assertEquals(valorEsperado, valor);
 	}
+	
+	@Test
+	@DisplayName("Coger temperatura")
+	void testCogerTemperatura() {
+		Double valorEsperado = 40.0;
+		termometro.ajustarTemperatura(35);
+		Double valor = termometro.getTemperaturaCelsius();
+		
+		assertEquals(valorEsperado, valor);
+	}
+	
+	
+	
+	
+	
 	
 
 }
